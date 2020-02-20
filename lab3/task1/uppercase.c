a3233b35b377aa2df4a635ad438be136a66f4c7c
Julia <jc34142n@pace.edu>
2020-02-19 19:29:29 -0500

    task1 complete!

--- lab3/task1/uppercase.c
+++ lab3/task1/uppercase.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
+ int main(void)
+ {
+
+   char str[10090];
    int ch, n = 0;
 
+printf("Please enter a letter or a string to be converted to uppercase:");
+
    while ((ch = getchar()) != EOF && n < 1000) {
-      str[n] = ch;
-      ++n;
+     if(ch >= 'a' && ch <= 'z'){
+       str[n] = ch - 32;
+       ++n;
+     }
+     else if(ch >= 'A' && ch <= 'Z'){
+       str[n] = ch;
+       ++n;
+     }
+     else{
+       str[n] = ch;
+       ++n;
+     }
+
    }
 
-   for (int i = 0; i < n; ++i)
+printf("\n");
+
+   for (int i = 0; i < n-1; ++i){
+
+
+
+
+
       putchar(str[i]);
+
+
+
+}
+
+   putchar('\n'); /* trailing '\n' needed in Standard C */
+
 
 
    return 0;
 
- */
-
-    return 0;
-}
-
+ }
