bf103ca3d68fa7eaae41238a5fd3fca88d0adda1
Muirrin <muiroconnell@gmail.com>
2020-03-24 22:21:11 -0400

    edit task3

--- lab5/task3/construct_3_structs_delete.c
+++ lab5/task3/construct_3_structs_delete.c
@@ -104,25 +104,37 @@ void delete_node_at(node_t ** head, int idx) {
 void delete_node_key(node_t **head, char * key) {
     //TODO: implement delete a node based on key
 	//given a certain key, find and delete.
+  /*
  int bool = 1;
  node_t * temp = *head;
-
   if(strcmp(temp->str,key)==0){
+    head = temp->next;
     free(temp);
-
   }
   else{
     while(bool==1){
     if(strcmp((temp->next)->str,key)==0)
-  {
+   {
       bool--;
     }
     temp = temp->next;
-
   }
-
   free(temp);
   }
+  */
+
+  node_t * temp = *head;
+
+     while(temp!=NULL){
+     if(strcmp(temp->str,key)==0)
+    {
+      free(temp);
+      temp = temp->next;
+      break;
+     }
+     temp = temp->next;
+   }
+
 }
 //You can ignore the following code for testing
 void dump_all(node_t*);
