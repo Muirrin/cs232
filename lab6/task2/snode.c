e9bf7772375e6569f734412a88db40b3ce47076f
Julia <jc34142n@pace.edu>
2020-03-27 17:07:50 -0400

    finished task2!

---
+++ lab6/task2/snode.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "snode.h"
+#include <string.h>
+#include <stdlib.h>
+
+struct snode *snode_create(char *s)
+{
+ //TODO: implement snode_create, change the prototype to
+ //match with header file
+
+ struct snode * n1= (struct snode*)malloc(sizeof(struct snode));
+ n1->str = (char*)malloc(sizeof(char)*(strlen(s)+1));
+ strcpy(n1->str, s);
+ n1->next = NULL;
+ return n1;
+/*
+struct snode *node;
+node = (struct snode*)malloc((strlen(s)+1) * sizeof(struct snode));
+temp->str = s;
+return temp;
+*/
+
+}
+void snode_destroy(struct snode * s)
+{
+ //TODO: implement snode_destroy
+free(s->str);
+free(s);
+
+
+}
