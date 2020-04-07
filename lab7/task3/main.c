cf84eef496b29e45c43be0fd2c565a347198de50
Julia <jc34142n@pace.edu>
2020-04-06 22:04:30 -0400

    task 3 done :)

--- lab7/task3/main.c
+++ lab7/task3/main.c
@@ -2,3 +2,5 @@ int main() {
     int a=13,b=5, c;
     c=a+b;
 }
+
+//for question 2, c = a * b, therefore c = 65
--- lab7/task3/main.s
+++ lab7/task3/main.s
@@ -1,21 +1,22 @@
 main:
   pushl %ebp
   movl %esp,%ebp
+  //initializing a, b ,c (3 integers)
   subl $12,%esp
   pushl %ebx
   pushl %esi
-  movl $13,-4(%ebp)
-  movl $5,-8(%ebp)
-  movl -4(%ebp),%eax
-  cltd
-  idivl -8(%ebp)
-  movl %edx,%esi
-  movl %eax,%ebx
-  movl -8(%ebp),%ebx
-  subl %esi,%ebx
-  movl -4(%ebp),%eax
-  imull %ebx,%eax
-  movl %eax,-12(%ebp)
+  movl $13,-4(%ebp) //a = 13
+  movl $5,-8(%ebp) // b = 5
+  movl -4(%ebp),%eax // 13 is stored in eax
+  cltd //converts 13 to double
+  idivl -8(%ebp) // eax = 13/5 edx = 13%5
+  movl %edx,%esi // esi = 13%5
+  movl %eax,%ebx // ebx = 13/5
+  movl -8(%ebp),%ebx // ebx = 5
+  subl %esi,%ebx // 13%5 - 5
+  movl -4(%ebp),%eax // eax =13
+  imull %ebx,%eax //eax =  5 * 13
+  movl %eax,-12(%ebp) // c = 65
   leal -20(%ebp),%esp
   popl %esi
   popl %ebx
