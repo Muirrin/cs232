3191e61fea25be97deab199830924097c425710e
Julia <jc34142n@pace.edu>
2020-04-07 21:17:52 -0400

    my changes to some of the tasks

--- lab7/task4/main.s
+++ lab7/task4/main.s
@@ -1,16 +1,16 @@
 main:
   pushl %ebp
   movl %esp,%ebp
-  subl $12,%esp
-  movl $13,-4(%ebp)
-  movl $5,-8(%ebp)
-  movl $0,-12(%ebp)
-  cmpl $6,-4(%ebp)
+  subl $12,%esp //grows stack by 12 bytes
+  movl $13,-4(%ebp) //stores 13 in a
+  movl $5,-8(%ebp) // stores 5 in b
+  movl $0,-12(%ebp) //stores 0 in c
+  cmpl $6,-4(%ebp) //  if(a>6)
   jle .L2
-  movl $15,-8(%ebp)
+  movl $15,-8(%ebp) // stores 15 in b
 .L2:
 .L3:
-  cmpl $14,-12(%ebp)
+  cmpl $14,-12(%ebp) // if 4< c
   jg .L4
   movl -12(%ebp),%eax
   addl %eax,-4(%ebp)
