57d58bc0e82e3e8ed0fab6d7e37c6a814f95bc4c
Muirrin <muiroconnell@gmail.com>
2020-04-08 14:33:37 -0400

    task4

--- lab7/task4/main.c
+++ lab7/task4/main.c
@@ -1,8 +1,26 @@
-int main() {
+/* int main() {
     int a=13,b=5, c=0;
-    if(a >6) {
-        c += 2;
+    if(a > 6) {
+       c += 2;
     } else {
         c -= 2;
     }
+}
+*/
+
+
+int main() {
+  int a, b, c;
+  a=13;
+  b=5;
+  c=0;
+
+  if(7<=a){
+      b=15;
+  }
+  while(15>c){
+    a = a+c;
+    c++;
+  }
+
 }
--- lab7/task4/main.s
+++ lab7/task4/main.s
@@ -5,17 +5,17 @@ main:
   movl $13,-4(%ebp) //stores 13 in a
   movl $5,-8(%ebp) // stores 5 in b
   movl $0,-12(%ebp) //stores 0 in c
-  cmpl $6,-4(%ebp) //  if(a>6)
-  jle .L2
+  cmpl $6,-4(%ebp) //  if(a>6) ((if(6<=a)))
+  jle .L2           //jump to .L2
   movl $15,-8(%ebp) // stores 15 in b
 .L2:
 .L3:
-  cmpl $14,-12(%ebp) // if 4< c
-  jg .L4
-  movl -12(%ebp),%eax
-  addl %eax,-4(%ebp)
-  incl -12(%ebp)
-  jmp .L3
+  cmpl $14,-12(%ebp) // if 14< c ((if14 > c))
+  jg .L4              //jump to .L4
+  movl -12(%ebp),%eax //stores c into %eax
+  addl %eax,-4(%ebp)  //adds %eax into a (a = a+c)
+  incl -12(%ebp)      //c++;
+  jmp .L3             //loops through L3 until
 .L4:
-  leave
+  leave               //ends the program
   ret
